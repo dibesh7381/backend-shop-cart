6a9c1a0d8639a3eccaaa6c495c543120603e06fd
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-08-17 18:19:04 +0530

    updated

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.json()); // parse JSON for PATCH requests
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Make sure uploads folder exists
