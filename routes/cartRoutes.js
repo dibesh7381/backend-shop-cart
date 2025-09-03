4e19dd0af9fb6934f6358b5925ba7aa62bf5f61a
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-09-04 00:11:31 +0530

    update routes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { addToCart, getCart, removeFromCart } from "../controllers/cartController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getProfile, updateProfile } from "../controllers/profileController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
