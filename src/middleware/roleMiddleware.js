b2adbcb9b76e320bc56c2a594529d1fd92cd115c
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-09-05 00:16:03 +0530

    update routes

--- src/middleware/roleMiddleware.js
+++
@@ -1,4 +0,0 @@
-export const isSeller = (req, res, next) => {
-  if (req.user?.role === "seller") return next();
-  return res.status(403).json({ message: "Access denied, only sellers allowed" });
-};
