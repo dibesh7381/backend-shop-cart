dfd6405b666c7676abae35af132ac5e08c746639
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-09-10 16:08:31 +0530

    update routes

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -12,11 +12,7 @@ export const getProfile = async (req, res) => {
 export const updateProfile = async (req, res) => {
   try {
     const { name } = req.body;
-    const updated = await Member.findByIdAndUpdate(
-      req.user.userId,
-      { name },
-      { new: true }
-    ).select("-password");
+    const updated = await Member.findByIdAndUpdate(req.user.userId, { name }, { new: true }).select("-password");
     res.json({ user: updated });
   } catch (err) {
     res.status(500).json({ message: "Error updating profile" });
@@ -41,19 +37,9 @@ export const updateProfile = async (req, res) => {
 export const updateProfilePic = async (req, res) => {
   try {
     if (!req.file) return res.status(400).json({ message: "No file uploaded" });
-
-    const updatedUser = await Member.findByIdAndUpdate(
-      req.user.userId,
-      { profilePic: req.file.path },
-      { new: true }
-    ).select("-password");
-
-    res.status(200).json({
-      message: "Profile picture updated successfully",
-      user: updatedUser,
-    });
+    const updatedUser = await Member.findByIdAndUpdate(req.user.userId, { profilePic: req.file.path }, { new: true }).select("-password");
+    res.json({ user: updatedUser });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ message: "Error updating profile picture" });
   }
 };
