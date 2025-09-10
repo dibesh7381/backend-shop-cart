9b63cf5393c94c1dbd1a44bc9a4c3fe4ab9bf43b
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-09-10 15:41:27 +0530

    update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware, isSeller } from "../middlewares/authMiddleware.js";
+import { authMiddleware, isSeller } from "../middleware/authMiddleware.js";
 import { upload } from "../utils/upload.js";
 import {
   createProduct,
